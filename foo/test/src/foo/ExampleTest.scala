1fa2dcf1c36066faee58a6612fe793e2ece4c54b
Nikos Maris <nmaris@crypteianetworks.com>
2018-07-25 10:08:48 +0300

    rollback to the scala integration test of the documentation

--- foo/test/src/foo/ExampleTest.scala
+++ foo/test/src/foo/ExampleTest.scala
@@ -5,27 +5,11 @@ import org.apache.flink.streaming.api.functions.sink.SinkFunction
 import org.apache.flink.api.common.functions.MapFunction
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment
 import scala.collection.mutable.ListBuffer
-import org.apache.flink.test.util.AbstractTestBase
+//import org.apache.flink.test.util.AbstractTestBase
 
-object ExampleTests extends AbstractTestBase {
-  val tests = Tests{
-    'test1 - {
-        val env = StreamExecutionEnvironment.getExecutionEnvironment
-        // configure your test environment
-        env.setParallelism(1)
-        // values are collected in a static variable
-        Companion.values.clear()
-        // create a stream of custom elements and apply transformations
-        env
-            .fromElements(1L, 21L, 22L)
-            .map(new MultiplyByTwo())
-            .addSink(new CollectSink())
-        // execute
-        env.execute()
-        // verify your results
-        assert(ListBuffer(2L, 42L, 44L) == Companion.values)
-    }
-  }
+object Companion {
+    // must be static
+    val values: ListBuffer[Long] = new ListBuffer[Long]()
 }
 /* UUT */
 class MultiplyByTwo extends MapFunction[Long, Long] {
@@ -44,7 +28,24 @@ class CollectSink extends SinkFunction[Long] {
     }
 }
 
-object Companion {
-    // must be static
-    val values: ListBuffer[Long] = new ListBuffer[Long]()
+object ExampleTests extends TestSuite {
+  val tests = Tests{
+    'test1 - {
+        val env = StreamExecutionEnvironment.getExecutionEnvironment
+        // configure your test environment
+        env.setParallelism(1)
+        // values are collected in a static variable
+        Companion.values.clear()
+        // create a stream of custom elements and apply transformations
+        env
+            .fromElements(1L, 21L, 22L)
+            .map(new MultiplyByTwo())
+            .addSink(new CollectSink())
+        // execute
+        env.execute()
+       // verify your results
+        assert(ListBuffer(2L, 42L, 44L) == Companion.values)
+    }
+  }
 }
+
